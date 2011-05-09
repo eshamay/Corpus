7eef92e9da89636e3f7028ec73703a4870d186a3
Eric Shamay <eshamay@uoregon.edu>
2011-05-09 14:46:17 -0700

    finished testing the xyz parsing routine

--- main.f90
+++ main.f90
@@ -5,8 +5,10 @@ use xyz_parsing_module
 type(xyz_file_type) :: xyz_file
 
 call xyz_OpenFile (1, "temp.xyz", xyz_file)
-call xyz_LoadNextFrame(xyz_file)
-
-write (*,*) xyz_file%num_atoms, xyz_file%atoms(4)
+do i=1,3
+  call xyz_LoadNextFrame(xyz_file)
+  write (*,*) xyz_file%num_atoms
+  write (*,*) xyz_file%atoms(4)
+end do
 
 end program tester
